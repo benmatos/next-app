cff8d0e17a52e6e55ce1354f9eef7e71096ceae6
benmatos <benjamim.matos@gmail.com>
2022-09-13 13:24:32 -0300

    inicial

---
+++ pages/Contatos.jsx
@@ -0,0 +1,10 @@
+import { Stack } from "@mui/system";
+import Layout from "./Layout";
+
+export default function Contato(){
+  return(
+    <Layout>
+      <Stack>Contatos</Stack>
+    </Layout>
+  )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import Layout from './Layout'
+
+export default function Home() {
+  return (
+    <Layout >
+      <div>Página Inicial</div>
+    </Layout>  
+  )
+}
